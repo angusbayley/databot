42a77ed12ce86128d5afcc47cc795f019ac9d794
Alina Solovjova <alinasolovjova@gmail.com>
2016-08-22 13:47:58 +0100

    Add MoM revenue graph

--- bigquery.py
+++ bigquery.py
@@ -6,6 +6,13 @@ from oauth2client.client import GoogleCredentials
 from utilities.parse_bigquery_data import parse_data
 import pandas as pd
 
+# plotting libraries
+import matplotlib
+matplotlib.use('Agg') # use a non-interactive backend for writing to file
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import seaborn as sns
+
 projectId = 'gc-data-warehouse'
 credentials = GoogleCredentials.get_application_default()
 
@@ -25,6 +32,15 @@ def revenue_summary():
     revenue_last_month['revenue_running_sum'] = revenue_last_month.revenue.cumsum()
     return revenue_this_month, revenue_last_month
 
+def generate_revenue_comment(revenue_this_month, revenue_last_month):
+    current_working_day = revenue_this_month.iloc[-1]['working_day']
+    current_revenue = int(revenue_this_month.iloc[-1]['revenue_running_sum'])
+    revenue_this_time_last_month = revenue_last_month.loc[revenue_last_month.working_day == current_working_day, 'revenue_running_sum']
+    revenue_this_time_last_month = int(revenue_this_time_last_month.values[0]) # get cell value
+    percent_mom_difference = round(100*(current_revenue - revenue_this_time_last_month)/revenue_this_time_last_month, 2)
+    comment_template = "So far this month we've generated £{0:,} in revenue, compared to £{1:,} this time last month ({2:+}%)."
+    return comment_template.format(current_revenue, revenue_this_time_last_month, percent_mom_difference)
+
 def run_query(query_filename):
     service = discovery.build('bigquery', 'v2', credentials=credentials)
 
@@ -38,11 +54,27 @@ def run_query(query_filename):
     response = request.execute()
     return parse_data(response['schema'], response['rows'])
 
+def plot_mom_chart(revenue_this_month, revenue_last_month):
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(FuncFormatter(_thousands_formatter))
+    plt.plot(revenue_this_month['working_day'], revenue_this_month['revenue_running_sum'], label='This Month')
+    plt.plot(revenue_last_month['working_day'], revenue_last_month['revenue_running_sum'], label='Last Month')
+    plt.xlabel('Working Day')
+    plt.ylabel('Revenue')
+    plt.legend(loc='lower right')
+    fig.savefig('mom_revenue.png')
+
 def _load_query(query_filename):
     with open(os.path.join('queries', query_filename)) as sql_file:
         return sql_file.read()
 
+def _thousands_formatter(x, pos):
+    'The args are the value and tick position'
+    return '£{}k'.format(int(x*1e-3))
+
 if __name__ == "__main__":
-    x, y = revenue_summary()
-    print(x, y)
+    this_month, last_month = revenue_summary()
+    comment = generate_revenue_comment(this_month, last_month)
+    print(comment)
+    plot_mom_chart(this_month, last_month)
 
